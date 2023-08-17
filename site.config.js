2d5ee33932bda2ccde585756382f6ce1dc53b201
SeongIkKim <kpic1638@gmail.com>
2023-08-18 02:27:10 +0900

    update config

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Seongik(Ike)",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    bio: "Finding Flow",
+    email: "kpic1638@gmail.com",
+    linkedin: "seongikkim",
+    github: "seongikkim",
+    instagram: "seongik_k",
   },
   projects: [
     {
@@ -18,8 +18,9 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Journey to find flow",
+    description: "Journal entries",
+    theme: "light"
   },
 
   // CONFIG configration (required)
@@ -47,9 +48,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+      repo: "seongikkim/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
